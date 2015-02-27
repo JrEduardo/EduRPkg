f5f14e00f7598087a675376fb48455c95a8a3f3a
Eduardo Junior <edujrrib@gmail.com>
2015-02-27 17:54:00 -0300

    Incluídas as novas funções

--- list.functions.R
+++ list.functions.R
@@ -7,7 +7,11 @@
 
 funs <- c(
     "https://raw.githubusercontent.com/JrEduardo/EduRPkg/master/rp.binom.R",
-    "https://raw.githubusercontent.com/JrEduardo/EduRPkg/master/rp.norm.R"
+    "https://raw.githubusercontent.com/JrEduardo/EduRPkg/master/rp.norm.R",
+    "https://raw.githubusercontent.com/JrEduardo/EduRPkg/master/rp.geom.R",
+    "https://raw.githubusercontent.com/JrEduardo/EduRPkg/master/rp.hyper.R",
+    "https://raw.githubusercontent.com/JrEduardo/EduRPkg/master/rp.nbinom.R",
+    "https://raw.githubusercontent.com/JrEduardo/EduRPkg/master/rp.pois.R"
 )
 
 
